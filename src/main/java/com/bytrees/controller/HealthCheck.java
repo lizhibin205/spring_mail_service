715c766301862a5899221ba5c93d07137a954c0a
zhibin02.li <zhibin02.li@vipshop.com>
2018-12-04 13:47:20 +0800

    log

--- src/main/java/com/bytrees/controller/HealthCheck.java
+++ src/main/java/com/bytrees/controller/HealthCheck.java
@@ -1,5 +1,7 @@
 package com.bytrees.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,11 @@ import com.bytrees.utils.ResponseJson;
 
 @RestController
 public class HealthCheck {
+	private static final Logger logger = LoggerFactory.getLogger(HealthCheck.class);
+
 	@RequestMapping(value = "/healthCheck", method = RequestMethod.GET, produces={"application/json;charset=UTF-8"})
 	public String index() {
+		logger.info("It works.");
 	    return (new ResponseJson<Object>(200, "success.", null)).toString();
 	}
 }
